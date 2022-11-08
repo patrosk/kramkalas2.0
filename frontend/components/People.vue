540aff0f3a930a383c6da8f05fdd1df41f156753
patrosk <naiade99@gmail.com>
2022-11-08 10:52:58 +0100

    add frontend

---
+++ frontend/components/People.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Välkommen till KRAMKALAS!</h1>
+    <h3>Här är alla välkomna!</h3>
+    <h5>{{ people.results }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    const api = "http://localhost:3000/api/v1/people";
+    this.people = await this.$axios.$get(api);
+  },
+  data() {
+    return {
+      people: {},
+    };
+  },
+};
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <People />
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
