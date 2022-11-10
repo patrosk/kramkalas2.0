81ff0dad9b47bfad23c95c4188378daca84f2bf4
patrosk <naiade99@gmail.com>
2022-11-10 15:12:21 +0100

    started on styling the header and footer components

---
+++ frontend/components/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <div class="footer-content">
+      <div>kramkalas logo round</div>
+      <ul>
+        <li>
+          kontakta oss
+          <ul>
+            <li>tel nr</li>
+            <li>mejl</li>
+          </ul>
+        </li>
+        <li>adress</li>
+        <ul>
+          <li>väg</li>
+          <li>postnummer</li>
+          <li>hitta hit länk</li>
+        </ul>
+        <li>sociala medier</li>
+        <ul>
+          <li>insta</li>
+          <li>facebook</li>
+        </ul>
+      </ul>
+    </div>
+    <p>© kramkalas 2022</p>
+  </footer>
+</template>
---
+++ frontend/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div class="logo">kramkalas logo</div>
+    <nav>
+      <div class="hamburger-menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="menu"></div>
+    </nav>
+  </header>
+</template>
